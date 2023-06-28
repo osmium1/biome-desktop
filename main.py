e73c5f2ee6ee33017fa2bb18af9321acf1ed2d16
osmium1 <om2003.inbox@gmail.com>
2023-06-29 00:58:15 +0530

    fixed duplicate window bug, renamed variables

--- main.py
+++ main.py
@@ -6,35 +6,39 @@ from pystray import MenuItem as item
 from CustomFont import RenderFont, render_text
 import sys
 
-# creating a flag to know if the settings dialog is open or not
-global settings_dialog_open
-settings_dialog_open = False
+# creating a flag to know if the config dialog is open or not
+global config_dialog_open
+config_dialog_open = False
 
 # creating a RenderFont object with the font file and the color of the text
 customfont1 = RenderFont(filename='fonts\Joystix Monospace.ttf', fill=(0, 0, 0))
 
 def on_exit():
-    # check if settings dialog is open 
-    if settings_dialog_open:
-        #display a message box to the user asking them to close the settings dialog first
-        tk.messagebox.showinfo('Biome', 'Please close the settings dialog first')
+    # check if config dialog is open 
+    if config_dialog_open:
+        #display a message box to the user asking them to close the config dialog first
+        tk.messagebox.showinfo('Biome', 'Please close the config window before exiting')
         return
     else: 
         print('Tray icon exited')
         menu_icon.stop()
         sys.exit()
 
-def on_settings():
-    open_settings_dialog()
+def on_config():
+    # check if config dialog is open
+    if config_dialog_open:
+        pass
+    else:
+        open_config_dialog()
     
 
-def open_settings_dialog():    
-    global settings_dialog_open
-    settings_dialog_open = True
+def open_config_dialog():    
+    global config_dialog_open
+    config_dialog_open = True
 
     # create the dialog window 
     dialog = tk.Tk()
-    dialog.title('Settings')
+    dialog.title('Biome Config')
     dialog.geometry('590x470')
     dialog.resizable(False, False)
 
@@ -45,15 +49,15 @@ def open_settings_dialog():
     
     # set the dialog open flag to false when user closes the dialog window
     def on_closing():
-        global settings_dialog_open
-        settings_dialog_open = False
+        global config_dialog_open
+        config_dialog_open = False
         dialog.destroy()
 
     dialog.protocol("WM_DELETE_WINDOW", on_closing)
     dialog.mainloop()
 
 menu = (  
-    item('Settings', on_settings),
+    item('Config', on_config),
     item('Exit', on_exit)
     )
 
